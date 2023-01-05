f4b892be7de880e7f3b870127819f1650ecf503b
Bosh-Kuo <evaktm1680@gmail.com>
2023-01-05 22:00:04 +0800

    download script

---
+++ download.sh
@@ -0,0 +1,9 @@
+# Data
+wget https://www.dropbox.com/s/edny8eheqoxwtgr/Data.zip?dl=1 -O ./Data.zip
+unzip Data.zip
+rm Data.zip
+
+# Inference models
+wget https://www.dropbox.com/s/t8fznw6jva92d76/Inference.zip?dl=1 -O ./Inference.zip
+unzip Inference.zip
+rm Inference.zip
